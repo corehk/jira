5008eb3fef7596a2f96f24bf4ccc5da9c5bc5436
ss <1915232691@qq.com>
2021-07-06 00:10:05 +0800

    feat:添加工程列表

--- src/screens/project-list/search-panel.jsx
+++ src/screens/project-list/search-panel.jsx
@@ -7,7 +7,7 @@ export const SearchPanel = ({ param, setParam, users }) => {
             })} />
             <select value={param.personId} onChange={evt => setParam({
                 ...param,
-                name: evt.target.value
+                personId: evt.target.value
             })}>
                 <option value="" >负责人</option>
                 {users.map((user) => <option key={user.id} value={user.id} >{user.name}</option>)}
