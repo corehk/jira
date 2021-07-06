85ac61859200d5c27011977f0ab0b3b3aab01ab0
ss <1915232691@qq.com>
2021-07-06 22:25:53 +0800

    feat:搜索增加防抖useDebounce

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,7 +1,7 @@
 import { List } from './list';
 import { SearchPanel } from './search-panel';
 import React, { useEffect, useState } from "react"
-import { cleanObject } from 'utils';
+import { cleanObject, useDebounce, useMount } from 'utils';
 import * as qs from 'qs';
 
 const apiUrl = process.env.REACT_APP_API_URL;
@@ -14,21 +14,23 @@ export const ProjectListScreen = () => {
         personId: ''
     })
 
+    const debouncedParam = useDebounce(param, 2000)
+
     useEffect(() => {
-        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response => {
             if (response.ok) {
                 setList(await response.json())
             }
         })
-    }, [param])
+    }, [debouncedParam])
 
-    useEffect(() => {
+    useMount(() => {
         fetch(`${apiUrl}/users`).then(async response => {
             if (response.ok) {
                 setUsers(await response.json())
             }
         })
-    }, [])
+    })
 
     return <div>
         <SearchPanel param={param} setParam={setParam} users={users} />
--- src/screens/project-list/list.jsx
+++ src/screens/project-list/list.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React from "react";
 export const List = ({ list, users }) => {
     return <table>
         <thead>
