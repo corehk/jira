12115038cc0b88aa4d729ebfacc9d9a9e809af4f
ss <1915232691@qq.com>
2021-07-15 07:54:51 +0800

    useAuth

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { loadDevTools } from 'jira-dev-tool';
+import { AppProviders } from 'context';
 
 loadDevTools(() => ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ <React.StrictMode>
+  <AppProviders>
+   <App />
+  </AppProviders>
+ </React.StrictMode>,
+ document.getElementById('root')
 ))
 
 
